e22dead7e635956a7196f53d3e94e06814b7a0a9
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-04-06 21:56:43 +0300

    bug/fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getContactsThunk } from '../redux/operations/contactsThunk';
 
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import { Container, Title } from './App.styles';
-// import { Loader } from './Loader/Loader';
 
 export function App() {
   const dispatch = useDispatch();
-  // const selectIsLoading = state => state.contacts.isLoading;
-  // const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(getContactsThunk());
@@ -23,7 +20,6 @@ export function App() {
       <ContactForm />
       <Title>Contacts</Title>
       <Filter />
-      {/* {isLoading ? <Loader /> : <ContactList />} */}
       <ContactList />
     </Container>
   );
